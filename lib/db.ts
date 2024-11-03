eb3d8b1bb5169ba4ad5243e08a20ea4239d20cc5
JeffersonGudiel <jeffersongudiel@icloud.com>
2024-11-03 16:27:10 -0600

    back changes lib/db

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // Evita que se redefina en un entorno donde no hay recarga de módulos
-  var prismaGlobal: PrismaClient | undefined;
-}
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const prisma = global.prismaGlobal || new PrismaClient();
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV !== "production") {
-  global.prismaGlobal = prisma;
-}
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
