d525ff0a9218b1fb4243f63916e379be793c9c01
JeffersonGudiel <jeffersongudiel@icloud.com>
2024-11-03 17:31:21 -0600

    correction url-list

--- components/url-list.tsx
+++ components/url-list.tsx
@@ -1,41 +1,35 @@
 "use client";
-
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Check, CopyIcon, EyeIcon } from "lucide-react";
 import { useEffect, useState } from "react";
-
 type Url = {
   id: string;
   shortCode: string;
   originalUrl: string;
   visits: number;
 };
-
 export default function UrlList() {
   const [urls, setUrls] = useState<Url[]>([]);
   const [copied, setCopied] = useState<boolean>(false);
   const [copieUrl, setCopieUrl] = useState<string>("");
-
-  // Definir directamente la base de la URL si la aplicación se ejecuta en el mismo dominio.
-  const shortenerUrl = (code: string) => `/${code}`;
-
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const shortenerUrl = (code: string) =>
+    `${process.env.NEXT_PUBLIC_BASE_URL}/${code}`;
   const fetchUrls = async () => {
+    setIsLoading(true);
     try {
-      // Uso de ruta relativa para hacer la solicitud.
-      const response = await fetch("/api/urls/route");
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
+      const response = await fetch("/api/urls");
       const data = await response.json();
       setUrls(data);
     } catch (error) {
       console.error("Error fetching URLs", error);
+    } finally {
+      setIsLoading(false);
     }
   };
-
   const handleCopyUrl = (code: string) => {
-    const fullUrl = window.location.origin + shortenerUrl(code); // Agrega el origen de la ventana.
+    const fullUrl = `${shortenerUrl(code)}`;
     navigator.clipboard.writeText(fullUrl).then(() => {
       setCopied(true);
       setCopieUrl(code);
@@ -45,10 +39,33 @@ export default function UrlList() {
       }, 3000);
     });
   };
-
   useEffect(() => {
     fetchUrls();
   }, []);
+
+  if (isLoading) {
+    return (
+      <div className="animate-pulse">
+        <div className="h-8 bg-gray-200 rounded w-1/4 mb-4"></div>
+        <ul>
+          {[1, 2, 3].map((num) => (
+            <li
+              key={num}
+              className="flex items-center gap-2 rounded-md border bg-card p-4 text-card-foreground justify-between">
+              <div className="h-4 bg-gray-200 rounded w-1/2"></div>
+              <div className="flex items-center gap-3">
+                <div className="h-5 w-5 bg-gray-200 rounded"></div>
+                <span className="flex items-center gap-2">
+                  <div className="h-4 w-4 bg-gray-200 rounded"></div>
+                  <div className="h-4 bg-gray-200 w-10 rounded"></div>
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -60,11 +77,10 @@ export default function UrlList() {
             className="flex items-center gap-2 justify-between bg-card rounded-md text-card-foreground border p-3">
             <Link
               href={`/${url.shortCode}`}
-              target="_blank"
+              target="blank"
               className="text-blue-500">
-              {window.location.origin + shortenerUrl(url.shortCode)}
+              {shortenerUrl(url.shortCode)}
             </Link>
-
             <div className="flex items-center">
               <Button
                 variant="ghost"
@@ -78,7 +94,6 @@ export default function UrlList() {
                 )}
                 <span className="sr-only">Copy URL</span>
               </Button>
-
               <span className="flex items-center gap-2">
                 <EyeIcon className="w-4 h-4" />
                 {url.visits} views
