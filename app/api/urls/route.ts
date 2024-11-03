39e871893c001edeba494e6efaf0fa6f5e56c1dd
JeffersonGudiel <jeffersongudiel@icloud.com>
2024-11-03 17:23:43 -0600

    correction api

--- app/api/urls/route.ts
+++ app/api/urls/route.ts
@@ -1,22 +1,18 @@
 import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    console.log("Fetching URLs...");
     const urls = await prisma.url.findMany({
       orderBy: { CreatedAt: "desc" },
       take: 5,
     });
-    console.log("URLs fetched successfully:", urls);
-
-    return new Response(JSON.stringify(urls), {
-      headers: { "Content-Type": "application/json" },
-    });
+    return NextResponse.json(urls);
   } catch (error) {
-    console.error("Error fetching URLs", error);
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+    console.error("Error fetching Urls", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
